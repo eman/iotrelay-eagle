98dc46f2bedc0805a4899c9048c9cdee20fca6d7
Emmanuel Levijarvi <emansl@gmail.com>
2014-05-16 11:01:34 -0700

    remove tuple return when no value is available

--- eagle_gateway.py
+++ eagle_gateway.py
@@ -26,13 +26,13 @@ class Poll(object):
 
     def get_readings(self):
         if datetime.datetime.now() < self.next_reading_time:
-            return ()
+            return
         try:
             timestamp, demand = self.gateway.get_instantaneous_demand()
         except GatewayError as e:
             logger.error(e)
             self.next_reading_time = datetime.datetime.now() + self.delta / 2
-            return ()
+            return
         if self.last_timestamp is None or timestamp != self.last_timestamp:
             logger.info("{0!s}, {1!s}".format(timestamp, demand))
             self.last_timestamp = timestamp
@@ -41,4 +41,3 @@ class Poll(object):
         else:
             logger.warning('duplicate reading')
             self.next_reading_time = datetime.datetime.now() + self.delta / 2
-            return ()
