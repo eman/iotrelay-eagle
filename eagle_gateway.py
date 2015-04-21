c9065cac1e737c43e23e86b6911be9f8c6a30fb6
Emmanuel Levijarvi <emansl@gmail.com>
2015-04-21 09:04:23 -0700

    summation fix-ups

--- eagle_gateway.py
+++ eagle_gateway.py
@@ -30,7 +30,7 @@ class Poll(object):
             return
         try:
             timestamp, demand = self.gateway.get_instantaneous_demand()
-            summation_vals = self.gateway.run_command('GET_SUMMATION_VALUES')
+            summ = self.gateway.run_command(name='GET_SUMMATION_VALUES')[-2]
         except GatewayError as e:
             logger.error(e)
             self.next_reading_time = datetime.datetime.now() + self.delta / 2
@@ -39,14 +39,13 @@ class Poll(object):
             logger.info("{0!s}, {1!s}".format(timestamp, demand))
             self.last_timestamp = timestamp
             self.next_reading_time = datetime.datetime.now() + self.delta
-            readings = Reading('power', demand, timestamp, self.series_key)
-            summation = summation_vals[-2]
-            if summation['TimeStamp'] == self.last_summation:
-                return readings
-            self.last_summation = summation['TimeStamp']
-            return [readings,
-                    Reading('power', summation['Value'],
-                            summation['TimeStamp'], 'summation')]
+            power = Reading('power', demand, timestamp, self.series_key)
+            if summ['TimeStamp'] == self.last_summation:
+                return power
+            self.last_summation = summ['TimeStamp']
+            return [power,
+                    Reading('power', summ['Value'], summ['TimeStamp'],
+                            'summation')]
         else:
             logger.warning('duplicate reading')
             self.next_reading_time = datetime.datetime.now() + self.delta / 2
